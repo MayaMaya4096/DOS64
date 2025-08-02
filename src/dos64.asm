2c85017749a930e1dc8f52f99620738908fc1f46
Maya F. Eroğlu <maya@x-plane.com>
2025-08-02 14:30:48 +0300

    Update and rename lmextender.asm to dos64.asm

--- src/dos64.asm
+++ src/dos64.asm
@@ -3,8 +3,8 @@ ORG 0x100
 SECTION .text
 
 %macro SET_INTERRUPT_ENTRY 1
-    push interrupt_entry_%{1}
-    push %{1}
+    push dword interrupt_entry_%{1}
+    push dword %{1}
     call set_idt_entry
     add sp, 4
 %endmacro
@@ -14,7 +14,7 @@ dos_entry:
 
     xchg bx, bx
 
-    mov sp, stack_top
+    mov esp, stack_top
     
     mov ah, 0x09
     mov dx, welcome_message
@@ -48,7 +48,7 @@ dos_entry:
     int 0x2F
 
     cmp al, 0x80
-    je .continue
+    jmp .continue
     
     mov ah, 0x09
     mov dx, no_xms_message
@@ -96,35 +96,30 @@ dos_entry:
     
         mov ah, 0x31
         mov al, 0
-        mov dx, stack_top - dos_entry + 16
+        mov dx, stack_top + 16
         shr dx, 4
         int 0x21
 
 set_idt_entry:
     BITS 16
 
-    push bp
-    mov bp, sp
-
     mov eax, idt
     xor ecx, ecx
-    mov cx, [bp + 4]
+    mov cl, [esp + 2]
     shl ecx, 4
     add eax, ecx
 
     mov ecx, [rm_base]
     xor edx, edx
-    mov dx, [bp + 6]
+    mov dx, [esp + 6]
     add ecx, edx
 
     mov [eax], cx
-    shr cx, 16
+    shr ecx, 16
     mov [eax + 6], cx
 
     mov word [eax + 2], 0x28
     mov word [eax + 4], 0b1000111000000001
-
-    pop bp
     
     ret
 
@@ -136,7 +131,7 @@ rm_services_entry:
     mov cs:[rm_stack], sp
     mov cs:[rm_stack + 2], ss
 
-    mov sp, stack_top
+    mov esp, stack_top
     mov ax, cs
     mov ss, ax
 
@@ -149,29 +144,20 @@ rm_services_entry:
     mov es, ax
     mov fs, ax
     mov gs, ax
-
-    mov ah, 0x05
-    call far [xms_call_address]
-
+    
     sgdt [original_gdtr]
     sidt [original_idtr]
-    
-    lgdt [lmextender_gdtr]
-    
+        
     call enter_pm32
     BITS 32
     
     call get_eip
-    mov eax, [eax + rm_base - $]
-        
-    pushf
-    and dword [esp], ~(1 << 14)
-    popf
+    lea eax, [eax + rm_stack - $]
     
     xor ecx, ecx
-    mov cx, [eax + rm_stack]
+    mov cx, [eax]
     xor edx, edx
-    mov dx, [eax + rm_stack + 2]
+    mov dx, [eax + 2]
     
     shl edx, 4
     add edx, ecx
@@ -189,11 +175,6 @@ rm_services_entry:
     call return_rm
     BITS 16
 
-    lgdt [original_gdtr]
-
-    mov ah, 0x06
-    call far [xms_call_address]
-
     pop gs
     pop fs
     pop es
@@ -210,8 +191,7 @@ rm_services_entry:
         call enter_lm
         BITS 64
 
-        mov ax, 0x30
-        ltr ax
+        call load_tr
         lidt [rel lmextender_idtr]
 
         mov eax, [rsp]
@@ -225,14 +205,16 @@ rm_services_entry:
         shl rdx, 4
         add rdx, rcx
         
-        sub rax, 6
+        mov cx, [rax + 4]
+        add rax, 6
 
         push 0x0
         push rax
         pushf
+        mov [rsp], cx
         push 0x28
         push rdx
-        
+
         iretq
 
 rm_services_handler:
@@ -241,12 +223,9 @@ rm_services_handler:
     mov eax, [esp + 4]
     
     cmp word [eax + 6], 0
-    je .setup
-
-    cmp word [eax + 6], 1
     je .enter
-    
-    .setup:
+
+    .enter:
         push edi
 
         mov edi, 0x200000
@@ -302,28 +281,21 @@ rm_services_handler:
         or eax, 1 << 8
         wrmsr
         
-        mov eax, 0
-        ret
-    
-    .enter:
         mov eax, 1
         ret
-
+        
 %macro INTERRUPT_ENTRY 1
     interrupt_entry_%{1}:
         BITS 64
+
+        cmp qword [rsp + 16], 0x28
+        je .error_code
+
+        push qword -1
         
-        push qword 0
-        push qword %{1}
-        jmp interrupt_handler
-%endmacro
-
-%macro INTERRUPT_ENTRY_ERROR_CODE 1
-    interrupt_entry_%{1}:
-        BITS 64
-
-        push qword %{1}
-        jmp interrupt_handler
+        .error_code:
+            push qword %{1}
+            jmp interrupt_handler
 %endmacro
 
 INTERRUPT_ENTRY 0
@@ -334,86 +306,258 @@ INTERRUPT_ENTRY 4
 INTERRUPT_ENTRY 5
 INTERRUPT_ENTRY 6
 INTERRUPT_ENTRY 7
-INTERRUPT_ENTRY_ERROR_CODE 8
+INTERRUPT_ENTRY 8
 INTERRUPT_ENTRY 9
-INTERRUPT_ENTRY_ERROR_CODE 10
-INTERRUPT_ENTRY_ERROR_CODE 11
-INTERRUPT_ENTRY_ERROR_CODE 12
-INTERRUPT_ENTRY_ERROR_CODE 13
-INTERRUPT_ENTRY_ERROR_CODE 14
+INTERRUPT_ENTRY 10
+INTERRUPT_ENTRY 11
+INTERRUPT_ENTRY 12
+INTERRUPT_ENTRY 13
+INTERRUPT_ENTRY 14
 INTERRUPT_ENTRY 16
-INTERRUPT_ENTRY_ERROR_CODE 17
+INTERRUPT_ENTRY 17
 INTERRUPT_ENTRY 18
 INTERRUPT_ENTRY 19
 INTERRUPT_ENTRY 20
-INTERRUPT_ENTRY_ERROR_CODE 21
+INTERRUPT_ENTRY 21
 
 INTERRUPT_ENTRY 129
 
 interrupt_handler:
     BITS 64
-
-    cmp dword [rsp], 32
-    jl .exception_handler
     
-    cmp dword [rsp], 48
-    jl .irq_handler
+    push gs
+    push fs
 
-    cmp dword [rsp], 129
+    sub rsp, 8
+    mov [rsp], es
+
+    sub rsp, 8
+    mov [rsp], ds
+
+    push r15
+    push r14
+    push r13
+    push r12
+    push r11
+    push r10
+    push r9
+    push r8
+    push rbp
+    push rdi
+    push rsi
+    push rdx
+    push rcx
+    push rbx
+    push rax
+    
+    cmp qword [rsp + 160], -1
+    jne .exception_handler
+    
+    cmp byte [rsp + 152], 8
+    jb .exception_handler
+    
+    cmp byte [rsp + 152], 16
+    jb .irq_handler
+
+    cmp byte [rsp + 152], 112
+    jb .exception_handler
+    
+    cmp byte [rsp + 152], 120
+    jb .irq_handler
+    
+    cmp byte [rsp + 152], 129
     je .lm_services
 
     .exception_handler:
         hlt
         
     .irq_handler:
-        call rm_interrupt
+        mov dil, [rsp + 152]
+        call rm_irq
 
         jmp .exit
 
-    .lm_services:
-        cmp dword [rsp + 40], 0
+    .lm_services:        
+        cmp rdi, 0
+        je .interrupt
 
-        je .exit
+        .interrupt:
+            mov al, sil
 
+            mov rsi, rdx
+            lea rdi, [rel rm_registers]
+            mov rcx, 38
+            rep movsb
+
+            mov dil, al
+            push rdx
+            call rm_interrupt
+            pop rdx
+            
+            lea rsi, [rel rm_registers]
+            mov rdi, rdx
+            mov rcx, 38
+            rep movsb
+
+            jmp .exit
+            
     .exit:
+        pop rax
+        pop rbx
+        pop rcx
+        pop rdx
+        pop rsi
+        pop rdi
+        pop rbp
+        pop r8
+        pop r9
+        pop r10
+        pop r11
+        pop r12
+        pop r13
+        pop r14
+        pop r15
+        
+        mov ds, [rsp]
+        add rsp, 8
+
+        mov es, [rsp]
+        add rsp, 8
+
+        pop fs
+        pop gs
+
         add rsp, 16
-        iret
+        iretq
 
 rm_interrupt:
     BITS 64
 
+    xchg bx, bx
+
+    push r15
+    push r14
+    push r13
+    push r12
+    push rbp
+    push rbx
+
+    mov [rel .interrupt + 1], dil
+    
     call return_pm32
     BITS 32
     
     call return_rm
     BITS 16
+    
+    mov eax, [rm_registers]
+    mov ebx, [rm_registers + 4]
+    mov ecx, [rm_registers + 8]
+    mov edx, [rm_registers + 12]
+    mov esi, [rm_registers + 16]
+    mov edi, [rm_registers + 20]
+    mov ebp, [rm_registers + 24]
 
-    lgdt [original_gdtr]
-    lidt [original_idtr]
+    mov ds, [rm_registers + 28]
+    mov es, [rm_registers + 30]
+    mov fs, [rm_registers + 32]
+    mov gs, [rm_registers + 34]
 
-    mov eax, [esp + 8]
-    mov byte [interrupt + 1], al
-
-    interrupt:
+    .interrupt:
         db 0xCD
         db 0
 
-    lgdt [lmextender_gdtr]
+    xchg bx, bx
+    
+    mov [rm_registers], eax
+    mov [rm_registers + 4], ebx
+    mov [rm_registers + 8], ecx
+    mov [rm_registers + 12], edx
+    mov [rm_registers + 16], esi
+    mov [rm_registers + 20], edi
+    mov [rm_registers + 24], ebp
+
+    mov [rm_registers + 28], ds
+    mov [rm_registers + 30], es
+    mov [rm_registers + 32], fs
+    mov [rm_registers + 34], gs
+    
+    pushf
+    pop ax
+    mov [rm_registers + 36], ax
+
+    mov ax, cs
+    
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
 
     call enter_pm32
     BITS 32
-    
+        
     call enter_lm
     BITS 64
-    
-    mov ax, 0x30
-    ltr ax
+        
+    call load_tr
     lidt [rel lmextender_idtr]
 
+    pop rbx
+    pop rbp
+    pop r12
+    pop r13
+    pop r14
+    pop r15
+
+    ret
+
+rm_irq:
+    BITS 64
+
+    push r15
+    push r14
+    push r13
+    push r12
+    push rbp
+    push rbx
+
+    mov [rel .irq + 1], dil
+
+    call return_pm32
+    BITS 32
+
+    call return_rm
+    BITS 16
+    
+    .irq:
+        db 0xCD
+        db 0
+
+    call enter_pm32
+    BITS 32
+
+    call enter_lm
+    BITS 64
+
+    call load_tr
+    lidt [rel lmextender_idtr]
+
+    pop rbx
+    pop rbp
+    pop r12
+    pop r13
+    pop r14
+    pop r15
+    
     ret
     
 enter_pm32:
     BITS 16
+    
+    mov ax, 0x2401
+    int 0x15
+    
+    lgdt [lmextender_gdtr]
     
     mov eax, [rm_base]
     
@@ -454,13 +598,13 @@ return_rm:
     .pm16:
         BITS 16
         
-        mov cx, 0x10
+        mov ax, 0x10
 
-        mov ds, cx
-        mov es, cx
-        mov fs, cx
-        mov gs, cx
-        mov ss, cx
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        mov ss, ax
 
         mov eax, [rm_base]
         sub esp, eax
@@ -484,23 +628,29 @@ return_rm:
             mov fs, ax
             mov gs, ax
             mov ss, ax
+
+            lgdt [original_gdtr]
+            lidt [original_idtr]
+
+            mov ax, 0x2400
+            int 0x15
             
             jmp ecx
   
 enter_lm:
     BITS 32
 
-    call get_eip
-    mov eax, [eax + rm_base - $]
-
-    mov [eax + jump_address], eax
-    add dword [eax + jump_address], .lm
-    mov word [eax + jump_address + 4], 0x28
-
     mov ecx, cr0
     or ecx, 1 << 31
     mov cr0, ecx
 
+    call get_eip
+    mov eax, [eax + rm_base - $]
+    
+    mov [eax + jump_address], eax
+    add dword [eax + jump_address], .lm
+    mov word [eax + jump_address + 4], 0x28
+    
     jmp far [eax + jump_address]
 
     .lm:
@@ -515,8 +665,10 @@ enter_lm:
 return_pm32:
     BITS 64
 
-    mov qword [rel jump_address], .cm32
+    lea rax, [rel .cm32]
+    mov [rel jump_address], rax
     mov word [rel jump_address + 8], 0x18
+    
     jmp far [rel jump_address]
 
     .cm32:
@@ -532,9 +684,18 @@ return_pm32:
         
         mov eax, cr0
         and eax, ~(1 << 31)
-        mov eax, cr0
+        mov cr0, eax
 
         ret 4
+
+load_tr:
+    BITS 64
+
+    mov ax, 0x30
+    ltr ax
+    and byte [rel gdt + 53], ~(1 << 1)
+    
+    ret
 
 get_eip:
     BITS 32
@@ -544,7 +705,7 @@ get_eip:
 
 SECTION .data
 
-welcome_message db "LMExtender x86_64 DOS Extender", "$"
+welcome_message db "DOS/64 64-Bit DOS Extender", "$"
 no_xms_message db "XMS services are not available!", "$"
 
 jump_address times 10 db 0
@@ -552,6 +713,7 @@ xms_call_address dd 0
 
 rm_base dd 0
 rm_stack dd 0
+rm_registers times 38 db 0
 
 original_gdtr times 6 db 0
 original_idtr times 6 db 0
@@ -565,14 +727,14 @@ gdt:
     dw 0xFFFF
     pm16_base_code times 3 db 0
     db 0b10011011
-    db 0b00001111
+    db 0
     db 0
 
     ;16-Bit Data Descriptor
     dw 0xFFFF
     pm16_base_data times 3 db 0
     db 0b10010011
-    db 0b00001111
+    db 0
     db 0
 
     ;32-Bit Code Descriptor
